f5c3cf7821a6cd7956b89a2390861ce25fa966ce
Laura <lhernan53112@gmail.com>
2020-07-06 14:25:46 -0400

    Added express body parsing and static content to server

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+// Parse body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static content from "public" directory
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
