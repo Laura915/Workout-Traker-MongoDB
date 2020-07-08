6f9f530f1b7d5206c7b733bce3b78311ec38053c
Laura <lhernan53112@gmail.com>
2020-07-08 16:24:02 -0400

    Created mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 // Parse body as JSON
@@ -10,6 +10,13 @@ app.use(express.json());
 
 // Serve static content from "public" directory
 app.use(express.static("public"));
+
+// Mongoose connection
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
