ab9f5d17016e36f2d84b4bb71c69eca9d56b7d03
Laura <lhernan53112@gmail.com>
2020-07-08 16:06:09 -0400

    Added totalDuration virtual, add the sum of all exercises durations

--- models/workout.js
+++ models/workout.js
@@ -1,32 +1,47 @@
+// Defines the Workout schema 
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [{
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }]
     },
-    exercises: [{
-        type: {
-            type: String
-        },
-        name: {
-            type: String
-        },
-        duration: {
-            type: Number
-        },
-        weight: {
-            type: Number
-        },
-        reps: {
-            type: Number
-        },
-        sets: {
-            type: Number
-        }
-    }],
+    //Include virtuals by default so mongoose can identify JSON.stringify
+    {
+        toJSON: { virtuals: true }
+    }
+);
+
+// Total duration virtual property
+workoutSchema.virtual("totalDuration").get(function(){
+    // Reduce function returns exercises array's sum
+    return this.exercises.reduce(function( total , exercise){
+        return total + exercise.duration;
+    }, 0 );
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
