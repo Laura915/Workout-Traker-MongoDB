96c5e5e8bb7db7858a29fddba471cf282b646433
Laura <lhernan53112@gmail.com>
2020-07-09 17:59:36 -0400

    Created put route that updates workout with exercises. Edited api routes to have catch block

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,23 +4,35 @@ const db = require("../models");
 
 // sends JSON of all workouts
 router.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
+    db.Workout.find()
         .then(dbWorkouts => {
             res.json(dbWorkouts);
         })
         .catch(err => {
             res.json(err);
-          });
+        });
 });
 
 // adds a new workout and sends it
 router.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body).then(dbWorkout => {
+    db.Workout.create(body)
+    .then(dbWorkout => {
         res.json(dbWorkout);
     })
-    .catch(err => {
-        res.json(err);
-      });
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+// add exercises to a workout, then sends updated workout
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 module.exports = router;
