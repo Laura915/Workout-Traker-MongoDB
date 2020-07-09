9b33542b6329ed8fb39ea41b3f615e8516be05b3
Laura <lhernan53112@gmail.com>
2020-07-08 22:12:53 -0400

    Created get route to get stat html page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,13 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Gets exercise html page 
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-
+// Gets stats html page 
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = router;
